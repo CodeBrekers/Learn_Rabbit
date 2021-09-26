4ce8e144114330328c429afab8948057907d188b
Beuni741 <it20241414@gmail.com>
2021-09-26 19:14:26 +0530

    add multiplication code

---
+++ app/src/main/java/com/example/project/DatabaseMain.java
@@ -0,0 +1,34 @@
+package com.example.project;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseMain extends SQLiteOpenHelper {
+    public static final String DBNAME = "multifactors.db";
+    public static final String TABLE_NAME = "factors";
+    public static final int VER= 1;
+
+    String query;
+
+    public DatabaseMain(@Nullable Context context) {
+        super(context, DBNAME, null, VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        query = "create table "+TABLE_NAME+"(id integer primary key, num1 text, num2 text, num3 text)";
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        query="drop table if exists "+TABLE_NAME+"";
+        db.execSQL(query);
+        onCreate(db);
+    }
+}
